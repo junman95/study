5300b10289f41c723af443747df551dd3f20a7c0
JunmanChoi <junman95@naver.com>
2018-05-18 15:02:55 +0900

    스터디
    dsfds

---
+++ mindtest.js
@@ -0,0 +1,16 @@
+function radio1_click(){
+    location.href="second.html";
+};
+function radio2_click(){
+    location.href="second.html";
+};
+
+function radio2_1_click(){
+    location.href="third.html";
+};
+function radio2_2_click(){
+    location.href="third.html";
+};
+function radio2_3_click(){
+    location.href="third.html";
+};
